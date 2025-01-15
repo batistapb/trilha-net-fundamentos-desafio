5754b3ae6b11b8b6f6701d10947e95c31b68de72
batistapb <batistadfd@gmail.com>
2025-01-15 18:59:51 -0300

    Pedindo para o usuário digitar quantas horas ele ficou e fazendo o cálculo de custo total do estacionamento

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -25,8 +25,6 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
             string placa = Console.ReadLine();
             placasArmazenadas.Add(placa);
 
@@ -35,12 +33,11 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
                 int horas = 0;
-                decimal valorTotal = 0; 
-
+                decimal valorTotal = 0;
+                Console.WriteLine("Digite quantas horas você ficou estacionado: ");
+                horas = Convert.ToInt32(Console.ReadLine()); 
+                valorTotal = precoInicial + (precoPorHora * horas);
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
 
